3e97b727f82ea775101e956b80ec107ddca38fef
Meitix <m.valizadeh@live.com>
2019-08-07 14:52:50 +0800

    upload codes.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.get("/mahdi", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/public/images/image.jpg"));
+});
+
+app.listen("7000");
+console.log("app is listening on port 7000");
